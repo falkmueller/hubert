8645695197c55c4df3083edda75e34d171695966
Falk Müller <git@falk-m.de>
2016-11-25 00:39:27 +0100

    BUGFIX: caching configuration

--- src/init.php
+++ src/init.php
@@ -1,10 +1,10 @@
 <?php
 
-function hubert($config = null)
+function hubert($config = null, $cache_file = null)
 {
     static $instance;
     if (!isset($instance)) {
-        $instance = new hubert\hubert($config);
+        $instance = new hubert\hubert($config, $cache_file);
     }
     return $instance;
 }
